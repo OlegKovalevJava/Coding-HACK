dd69f1e3177c2a60a5ca3f249ef331e8693195df
Oleg <kovalyovop@yandex.ru>
2022-03-03 18:00:32 +0300

    add springBoot

---
+++ src/main/java/hackathon/springjpapostgres/SpringjpapostgresApplication.java
@@ -0,0 +1,13 @@
+package hackathon.springjpapostgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringjpapostgresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringjpapostgresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hackathon/springjpapostgres/SpringjpapostgresApplicationTests.java
@@ -0,0 +1,13 @@
+package hackathon.springjpapostgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringjpapostgresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
